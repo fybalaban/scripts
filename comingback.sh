c0b47da0e8378644ccc07ec4a71758136d6d9ead
Ferit Yiğit BALABAN <fyb@duck.com>
2022-03-31 17:56:03 +0300

    Create helper script to automate log on tasks

---
+++ comingback.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+#       Ferit Yigit BALABAN <fyb@duck.com>, 2022
+#
+
+# Step 1: Screen brightness back at %50
+brightnessctl --device amdgpu_bl0 set 50%
+
+# Step 2: Set keyboard backlight to %33
+brightnessctl --device asus::kbd_backlight set 33%
+
+# Step 3: Set mouse light to breathing colors
+rivalcfg --color=#FF66F5 --light-effect=breath
