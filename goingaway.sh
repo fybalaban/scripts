4974b61c33976381d191d0b1e421e34d62eca8c5
Ferit Yiğit BALABAN <fyb@duck.com>
2022-03-31 17:55:22 +0300

    Create helper script to automate log off tasks

---
+++ goingaway.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+#       Ferit Yigit BALABAN <fyb@duck.com>, 2022
+#
+
+# Step 1: Set screen brightness to 0
+brightnessctl --device amdgpu_bl0 set 0
+
+# Step 2: Set keyboard backlight to 0
+brightnessctl --device asus::kbd_backlight set 0
+
+# Step 3: Set mouse light to 0
+rivalcfg --color=#000000 --light-effect=steady
+
+# Step 4: Set volume level to 0
+pactl set-sink-volume @DEFAULT_SINK@ 0%
+
+# Step 5: Lock the screen with screensaver
+betterlockscreen --lock --off 10
