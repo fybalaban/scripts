f21f3f71f03c2cdb800d3f16b569e9e985564eda
Ferit Yiğit BALABAN <fyb@duck.com>
2022-04-22 15:11:26 +0300

    Fix error message when nothing is playing

--- nowplaying.sh
+++ nowplaying.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-if [ $(playerctl status) = 'Playing' ]; then
+status=$( playerctl status )
+if [ $? -eq 0 ]; then
     echo $( playerctl -f '{{trunc(xesam:artist, 20)}} - {{trunc(xesam:title, 30)}}' metadata )
+else
+    echo ""
 fi
 exit 0
