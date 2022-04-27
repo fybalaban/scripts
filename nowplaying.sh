1be65ef5c060c7fd2c135599e0f753180ae9b9f6
Ferit Yiğit BALABAN <fyb@duck.com>
2022-04-28 02:23:28 +0300

    Better nowplaying script in Bash

--- nowplaying.sh
+++ nowplaying.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-status=$( playerctl status )
+s=$( playerctl -f '{{trunc(xesam:artist, 20)}} - {{trunc(xesam:title, 30)}}' metadata )
 if [ $? -eq 0 ]; then
-    echo $( playerctl -f '{{trunc(xesam:artist, 20)}} - {{trunc(xesam:title, 30)}}' metadata )
+    echo "$s"
 else
     echo ""
 fi
