2c468b5cd75a17d1ae1915f1da45ab9148772cda
Ferit Yiğit BALABAN <fyb@duck.com>
2022-06-07 15:27:35 +0300

    Simplify comparer

--- compare.py
+++ compare.py
@@ -2,43 +2,40 @@
 #
 #       Ferit Yiğit BALABAN <fyb@duck.com>, 2022
 #
-import urllib.request
 
 
-# noinspection PyUnreachableCode
 def main():
-    if False:
-        follower_path = '/home/ferit/Downloads/followers.csv'
-        following_path = '/home/ferit/Downloads/following.csv'
+    follower_path = '/home/ferit/scripts/follower'
+    followed_path = '/home/ferit/scripts/followed'
+    follower2_path = '/home/ferit/scripts/follower2'
+    with open(follower_path, 'r') as f:
+        follower_list = f.readlines()
+        f.close()
+    with open(followed_path, 'r') as f:
+        followed_list = f.readlines()
+        f.close()
+    with open(follower2_path, 'r') as f:
+        follower2_list = f.readlines()
+        f.close()
 
-        with open(follower_path, 'r') as f:
-            follower_list = f.readlines()
-            f.close()
-        with open(following_path, 'r') as f:
-            following_list = f.readlines()
-            f.close()
+    follower_list = [x.removesuffix('\n') for x in follower_list]
+    followed_list = [x.removesuffix('\n') for x in followed_list]
+    follower2_list = [x.removesuffix('\n') for x in follower2_list]
 
-        follower_list.remove(follower_list[0])
-        following_list.remove(following_list[0])
+    print('Old method:')
+    for followed in followed_list:
+        if followed not in follower1_list:
+            print(followed, 'doesn\'t follow you back.')
 
-        follower_list = [line.split(',')[1] for line in follower_list]
-        following_list = [line.split(',')[1] for line in following_list]
-    else:
-        follower_path = '/home/ferit/scripts/follower'
-        followed_path = '/home/ferit/scripts/followed'
-        with open(follower_path, 'r') as f:
-            follower_list = f.readlines()
-            f.close()
-        with open(followed_path, 'r') as f:
-            followed_list = f.readlines()
-            f.close()
+    print('New method:')
+    for followed in followed_list:
+        if followed not in follower2_list:
+            print(followed, 'doesn\t follow you back.')
 
-        follower_list = [x.removesuffix('\n') for x in follower_list]
-        followed_list = [x.removesuffix('\n') for x in followed_list]
-
-        for followed in followed_list:
-            if followed not in follower_list:
-                print(followed, 'doesn\'t follow you.')
+    print('Who unfollowed me:')
+    for old_follower in follower_list:
+        if old_follower not in follower2_list:
+            print(old_follower, 'unfollowed you.')
 
 
 if __name__ == '__main__':
