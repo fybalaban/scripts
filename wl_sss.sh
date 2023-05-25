600f6e3a0889bd85fb59717f8e3a8521a0662080
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-05-25 07:37:26 +0300

    hyprland workspace: select rectangular, ss and save

---
+++ wl_sss.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#
+#       Ferit Yiğit BALABAN, <fybalaban@fybx.dev>
+#
+#       Select an area with slurp, ss with grim 
+#       and copy to clipboard.
+SS="$HOME/items/ss"
+grim -g "$( slurp )" - | wl-copy --type image/png && wl-paste > "$SS"/$( date +'%Y-%m-%d-%H%M%S.png' )
