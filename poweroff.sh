8b00ac90eb01b0eaf1a963755e5464e8e4882bc4
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-11-10 21:04:28 +0300

    Remove unison sync and add resync_vault.py to poweroff and reboot scripts
    Signed-off-by: Ferit Yiğit BALABAN <fybalaban@fybx.dev>

--- poweroff.sh
+++ poweroff.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 dt=$(date -Iseconds)
-unison=$($HOME/scripts/unison_sync.sh)
+# unison=$($HOME/scripts/unison_sync.sh)
 rm "$HOME/.cache/cliphist/db"
+python "$HOME/scripts/resync_vault.py"
 echo "[$dt] INFO: navi, powering off" >> navi.log
 poweroff
