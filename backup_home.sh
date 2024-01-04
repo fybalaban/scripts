a65b8bf190a46b7977b452864f4b388c9e09a592
Ferit Yiğit BALABAN <fyb@fybx.dev>
2024-01-04 21:50:45 +0300

    better logic

--- backup_home.sh
+++ backup_home.sh
@@ -3,9 +3,17 @@
 #       Ferit Yiğit BALABAN <fybalaban@fybx.dev>, 2023
 #
 EXCLUDE_FILE="$HOME/.backupexclude"
+FILE="$HOME-$( date +'%y%m%d' ).tar.gz)"
+USER="$( whoami )"
 
-echo "Hello, $( whoami )"
+echo "Hello, $USER"
 echo "with excluded folders: $( du -sh --exclude-from="$EXCLUDE_FILE" "$HOME" )"
 echo "with everything      : $( du -sh "$HOME" )"
+echo "file will be saved as $FILE"
 
-sudo tar --exclude-from="$EXCLUDE_FILE" --create --verbose --preserve-permissions --gzip --file "$HOME-$( date +'%y%m%d' ).tar.gz" "$HOME"
+sudo tar --exclude-from="$EXCLUDE_FILE" \ 
+  --create --verbose --preserve-permissions \ 
+  --gzip --file "$FILE" "$HOME"
+
+sudo chown "$USER":"$USER" "$FILE"
+
