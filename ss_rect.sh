e6f13164a891c5c486427c1e6797785c19735ba7
Ferit Yiğit BALABAN <fyb@fybx.dev>
2024-02-19 14:47:46 +0300

    don't save the file if nothing was shot

--- ss_rect.sh
+++ ss_rect.sh
@@ -5,4 +5,13 @@
 #       Select an area with slurp, ss with grim 
 #       and copy to clipboard.
 SS="/media/fyb/share/shoka/swap/screenshots"
-grim -c -g "$( slurp )" - | wl-copy --type image/png && wl-paste > "$SS"/$( date +'%Y-%m-%d-%H%M%S.png' )
+TEMP_FILE="$HOME/temporary_screenshot"
+
+grim -c -g "$( slurp )" "$TEMP_FILE"
+
+if [ $? -eq 0 ]; then
+  wl-copy --type image/png < "$TEMP_FILE"
+  wl-paste > "$SS/$( date +'%Y-%m-%d-%H%M%S.png' )" 
+  rm "$TEMP_FILE"
+fi
+
